b615b75fa545ac04957f0138698fadd59d8a37ba
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-07-27 17:09:49 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,20 @@ from .forms import BookForm
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
+from .forms import BookForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():  # Validates and sanitizes input
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
