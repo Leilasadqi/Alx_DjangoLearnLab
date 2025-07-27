12d9dd754785f7177e7cbc5aaed040c2c17ecdc8
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-07-27 16:58:13 +0100

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,10 +1,12 @@
 INSTALLED_APPS = [
-    AUTH_USER_MODEL = 'accounts.CustomUser'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
+    'bookshelf',    # or your app name like 'accounts'
+    'accounts',     # add your custom user app here
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'  # <-- outside INSTALLED_APPS
