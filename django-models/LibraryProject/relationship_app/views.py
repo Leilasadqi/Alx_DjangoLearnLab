9df8cf55a3de35ffce66f5279c1dc901eb2068e9
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-07-27 16:09:38 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from .models import Book, Library
 from .forms import BookForm
-
 # ------------------------------
 # General Views
 # ------------------------------
