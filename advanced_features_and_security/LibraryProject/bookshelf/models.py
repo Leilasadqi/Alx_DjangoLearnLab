5ecbb6f1338de3382e1f5751dc0114e9a11a7a3d
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-07-27 17:00:33 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -34,3 +34,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
