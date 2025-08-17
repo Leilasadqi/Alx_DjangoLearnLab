d64947c32d7cfbc00b9ea9ea16f3f17ad6095078
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-08-17 20:03:03 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,7 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    PostByTagListView,  
     register,
     profile,
     CommentCreateView,
@@ -28,6 +29,9 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # Tags
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),  
 
     # Comments
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
