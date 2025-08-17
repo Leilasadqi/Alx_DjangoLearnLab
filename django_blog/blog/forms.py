d41e7c65acfc7d67742418fcb6841a8af9cc1df1
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-08-17 19:55:07 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -6,6 +6,7 @@ from django import forms
 from .models import Post
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -18,7 +19,7 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ['title', 'content', 'tags']
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
