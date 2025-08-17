0871ab111c0a8232ed23157ff891c1346ba35c94
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-08-17 16:06:28 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,16 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django import forms
 from .models import Post
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment...'}),
+        }
+
 
 class PostForm(forms.ModelForm):
     class Meta:
