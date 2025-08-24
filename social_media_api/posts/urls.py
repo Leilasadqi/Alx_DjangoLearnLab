6c5df94df5e4da01ec98e8e61bbf7020ccc7ddf8
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-08-24 19:15:02 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from .views import PostViewSet, CommentViewSet, FeedView
 
-
+# DRF routers for Post and Comment viewsets
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
-
-urlpatterns = router.urls
+# Add feed route
+urlpatterns = router.urls + [
+    path('feed/', FeedView.as_view(), name='feed'),
+]
