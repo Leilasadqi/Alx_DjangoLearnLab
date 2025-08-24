3e5f125c3800d45a42b67254ffb948792e436774
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-08-24 18:56:25 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,47 +3,37 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
-
-
-
+# Pagination class
 class StandardResultsSetPagination(PageNumberPagination):
-page_size = 5
-page_size_query_param = 'page_size'
-max_page_size = 50
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
-
-
-
+# Custom permission: only authors can edit/delete
 class IsAuthorOrReadOnly(permissions.BasePermission):
-def has_object_permission(self, request, view, obj):
-if request.method in permissions.SAFE_METHODS:
-return True
-return obj.author == request.user
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
-
-
-
+# Post CRUD
 class PostViewSet(viewsets.ModelViewSet):
-queryset = Post.objects.all().order_by("-created_at")
-serializer_class = PostSerializer
-permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-pagination_class = StandardResultsSetPagination
-filter_backends = [filters.SearchFilter]
-search_fields = ["title", "content"]
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "content"]
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
-def perform_create(self, serializer):
-serializer.save(author=self.request.user)
-
-
-
-
+# Comment CRUD
 class CommentViewSet(viewsets.ModelViewSet):
-queryset = Comment.objects.all().order_by("-created_at")
-serializer_class = CommentSerializer
-permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-pagination_class = StandardResultsSetPagination
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = StandardResultsSetPagination
 
-
-def perform_create(self, serializer):
-serializer.save(author=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
