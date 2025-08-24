bd9f2a00d0dbc628142d48f24025382d82634fd1
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2025-08-24 19:11:14 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, generics
 from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -37,3 +37,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+# Feed view: posts from followed users
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        # Get users the current user is following
+        following_users = self.request.user.following.all()
+        # Return posts from followed users, newest first
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
